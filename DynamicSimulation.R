2531745ced4299e65c2de460df0d6eedcf177a65
Sandy Porto <sandyporto@gmail.com>
2016-06-26 15:10:24 -0300

    New way of forming dy.data
    dy.data contains the values also

--- DynamicSimulation.R
+++ DynamicSimulation.R
@@ -1,20 +1,23 @@
 
-prepare.dy.data <- function(ndata,pdatainicial){
+prepare.dy.data <- function(data,pdatainicial,ndata=nrow(data)){
   initial.ndata = round(ndata*pdatainitial)
   
   dy.data = as.matrix(sample(1:ndata,initial.ndata,replace=F))
+  dy.data = cbind(dy.data,data[dy.data,])
   return(dy.data)
 }
 
 
 
-add.object <- function(dy.d,ndata){
+add.object <- function(dy.d,data,ndata=nrow(data)){
   if(ndata==nrow(dy.d)){return(dy.d)}
-  indexes.candidates = which(!c(1:ndata) %in% dy.d)
+  indexes.candidates = which(!c(1:ndata) %in% dy.d[,1])
   if(length(indexes.candidates)>=2){
-    dy.d = rbind(dy.d,sample(indexes.candidates,1))
+    aux = sample(indexes.candidates,1)
+    dy.d = rbind(dy.d,c(aux,data[aux,]))
   }else{
-    dy.d = rbind(dy.d,indexes.candidates)
+    aux = indexes.candidates
+    dy.d = rbind(dy.d,c(aux,data[aux,]))
   }
   return(dy.d)
 }
@@ -27,13 +30,13 @@ delete.object <- function(dy.d){
 }
 
 
-trigger.change <- function(dy.d,ndata,padd = 0.5,pdelete = 0.5){
+trigger.change <- function(dy.d,data,ndata=nrow(data),padd = 0.5,pdelete = 0.5){
   pchange = c(padd,pdelete)
   pchange = pchange/sum(pchange)
   
   type.change = sample(c("add","delete"),1,prob=pchange)
   if(type.change=="add"){
-    dy.d = add.object(dy.d,ndata)
+    dy.d = add.object(dy.d,data)
   }else{
     if(type.change=="delete"){
       dy.d = delete.object(dy.d)
--- testDynamicSimulation.R
+++ testDynamicSimulation.R
@@ -1,30 +1,30 @@
 
 
 
-dy.data = prepare.dy.data(ndata,pdataincial)
+dy.data = prepare.dy.data(data,pdataincial)
 cat("\n")
 
 test_that("add.object function",{
-  new.dy.data = trigger.change(dy.data,ndata,padd=1,pdelete=0)
+  new.dy.data = trigger.change(dy.data,data,padd=1,pdelete=0)
   expect_gte(nrow(new.dy.data),nrow(dy.data))
   expect_lte(nrow(new.dy.data),ndata)
-  expect_true(length(unique(new.dy.data))==nrow(new.dy.data))
+  expect_true(length(unique(new.dy.data[,1]))==nrow(new.dy.data))
 })
 
 
 test_that("delete.object function",{
-  new.dy.data = trigger.change(dy.data,ndata,padd=0,pdelete=1)
+  new.dy.data = trigger.change(dy.data,data,padd=0,pdelete=1)
   expect_lte(nrow(new.dy.data),nrow(dy.data))
   expect_gte(nrow(new.dy.data),1)
-  expect_true(length(unique(new.dy.data))==nrow(new.dy.data))
+  expect_true(length(unique(new.dy.data[,1]))==nrow(new.dy.data))
 })
 
 
 for(i in 1:ndata){
-  dy.data = trigger.change(dy.data,ndata)
+  dy.data = trigger.change(dy.data,data)
   test_that("",{
     expect_lte(nrow(dy.data),ndata)
     expect_gte(nrow(dy.data),1)
-    expect_true(length(unique(dy.data))==nrow(dy.data))
+    expect_true(length(unique(dy.data[,1]))==nrow(dy.data))
   })
 }
